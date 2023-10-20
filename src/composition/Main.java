ed370e673a90c11105976b43c12bedef244638d3
MaryuriFernandez <142600040+MaryuriFernandez@users.noreply.github.com>
2023-10-20 08:55:06 -0500

    Add files via upload
    Composición: relacion entre dos clases, donde una compone o es parte de la otra clase, en este proyecto el motor compone al coche, puesto que cuando se crea un coche, inmediatamente se crea el motor, ya que hacer parte de el, además no existe independientemente, pues si se elimina al coche desaparece el motor también.

---
+++ src/composition/Main.java
@@ -0,0 +1,15 @@
+
+package composition;
+
+public class Main {
+
+    public static void main(String[] args) {
+         // Crear un coche
+        Coche miCoche = new Coche();
+
+        // Arrancar y apagar el coche
+        miCoche.arrancar();
+        miCoche.apagar();
+    }
+    
+}
