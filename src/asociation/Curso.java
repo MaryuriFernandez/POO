ffe6d001b5d7c2274e0c025322c70d26c3503454
MaryuriFernandez <142600040+MaryuriFernandez@users.noreply.github.com>
2023-10-20 08:40:51 -0500

    Add files via upload
    Asociación: relacion entre dos clases, en este proyecto la relacion existe entre Curso y Estudiante, puesto que un estudiante puede tener de 0 a muchos cursos, pero puede ser estudiante sin tenerlos

---
+++ src/asociation/Curso.java
@@ -0,0 +1,14 @@
+
+package asociation;
+
+public class Curso {
+     private String nombre;
+
+    public Curso(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
